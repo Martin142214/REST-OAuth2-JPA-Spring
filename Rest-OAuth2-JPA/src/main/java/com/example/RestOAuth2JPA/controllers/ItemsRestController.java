07bfb177dbe6df895614d276e3d43f79b177b5e2
Martin142214 <mi_kalonkin@mail.bg>
2023-08-18 18:19:09 +0300

    update ItemsRestController -- from RestController to Controller

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/ItemsRestController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/ItemsRestController.java
@@ -1,11 +1,11 @@
 package com.example.RestOAuth2JPA.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(path = "/home")
 public class ItemsRestController {
 
