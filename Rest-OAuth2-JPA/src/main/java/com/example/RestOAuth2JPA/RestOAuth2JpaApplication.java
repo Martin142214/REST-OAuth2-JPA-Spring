228e7be14d274cda2a7d9e87dd897934fd68ca21
Martin142214 <mi_kalonkin@mail.bg>
2023-08-18 17:26:00 +0300

    add "main" method of the app

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/RestOAuth2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestOAuth2JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestOAuth2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestOAuth2JpaApplication.class, args);
+	}
+
+}
---
+++ Rest-OAuth2-JPA/src/test/java/com/example/RestOAuth2JPA/RestOAuth2JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestOAuth2JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOAuth2JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
