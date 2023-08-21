ae799cdad0858f1f013cc5c1c713da82c9bf3ec2
Martin142214 <mi_kalonkin@mail.bg>
2023-08-21 18:05:50 +0300

    add exclude for data source in "main" class

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/RestOAuth2JpaApplication.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/RestOAuth2JpaApplication.java
@@ -2,11 +2,13 @@ package com.example.RestOAuth2JPA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) 
 public class RestOAuth2JpaApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RestOAuth2JpaApplication.class, args);
 	}
 
