9a1e0a6e7706be0dc78019b27020ad27a4e7ccbb
Martin142214 <mi_kalonkin@mail.bg>
2023-08-21 18:04:55 +0300

    update UserDetailsService class -- loadByUsername() returns mock user

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/UserDetailsServiceImplementation.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/UserDetailsServiceImplementation.java
@@ -1,16 +1,19 @@
 package com.example.RestOAuth2JPA.services;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import com.example.RestOAuth2JPA.DTO.entities.UserDetailsImplement;
-
+@Service
 public class UserDetailsServiceImplementation implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new UserDetailsImplement("user");
+        return new User("user", "user", new ArrayList<>());
     }
     
 }
