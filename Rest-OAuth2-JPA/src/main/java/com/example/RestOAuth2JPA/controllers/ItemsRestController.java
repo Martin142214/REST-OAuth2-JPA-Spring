28329d14630fe3793100f1b3db4dd7c7d1db8246
Martin142214 <mi_kalonkin@mail.bg>
2023-08-22 15:16:20 +0300

    refactor authenticate() in ItemsRestController

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/ItemsRestController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/ItemsRestController.java
@@ -1,33 +1,27 @@
 package com.example.RestOAuth2JPA.controllers;
 
-import javax.security.sasl.AuthenticationException;
-
-import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.RestOAuth2JPA.DTO.entities.UserDetailsImplement;
+
 import com.example.RestOAuth2JPA.DTO.entities.classModels.AuthenticationRequest;
 import com.example.RestOAuth2JPA.DTO.entities.classModels.AuthenticationResponse;
 import com.example.RestOAuth2JPA.services.JwtUtil;
 import com.example.RestOAuth2JPA.services.UserDetailsServiceImplementation;
 
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ItemsRestController {
 
     @Autowired AuthenticationManager authenticationManager;
@@ -41,25 +35,32 @@ public class ItemsRestController {
         
     }
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse response) throws Exception {
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         var username = authenticationRequest.getUsername();
         var password = authenticationRequest.getPassword();
-
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (Exception e) {
-            throw new Exception("Not a valid username or password");
-        }
+        
+        this.authenticate(username, password);
         final UserDetails userDetails = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
 
         var token = jwtUtil.generateToken(userDetails);
 
-        Cookie cookie = new Cookie("Auth", token);
-        cookie.setMaxAge(30*6);
-        response.addCookie(cookie);
+        //Cookie cookie = new Cookie("Auth", token);
+        //cookie.setMaxAge(30*6);
+        //response.addCookie(cookie);
 
         return ResponseEntity.ok(new AuthenticationResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
     @GetMapping("/home")
