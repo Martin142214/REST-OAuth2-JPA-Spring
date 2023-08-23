d0cf0f9b97daca1ef3d687a38984876afba08a64
Martin142214 <mi_kalonkin@mail.bg>
2023-08-23 13:02:21 +0300

    add LoginController -- return login template in login()

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.RestOAuth2JPA.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    public LoginController() {
+        
+    }
+    
+    @GetMapping
+    public String login() {
+        return "login.html";
+    }
+}
