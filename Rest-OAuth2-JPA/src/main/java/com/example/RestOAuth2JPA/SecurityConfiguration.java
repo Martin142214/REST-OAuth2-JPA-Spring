1ab1c766698b70aa87ce9c274b1580c07ad7671d
Martin142214 <mi_kalonkin@mail.bg>
2023-08-23 14:51:40 +0300

    change filterchain() path restrictions

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/SecurityConfiguration.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -81,7 +82,8 @@ public class SecurityConfiguration{
 
         return http.build();*/
         http.csrf().disable()
-                    .authorizeRequests().requestMatchers("/api/v1/authenticate").permitAll()
+                    .authorizeRequests().requestMatchers("/authenticate").permitAll()
+                    .requestMatchers(HttpMethod.GET,"/login").permitAll()
                     .anyRequest().authenticated()
                     .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
