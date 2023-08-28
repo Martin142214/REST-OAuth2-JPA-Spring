e7748f8870485b88fc7f015ce4907ee83cfc4408
Martin142214 <mi_kalonkin@mail.bg>
2023-08-28 15:18:13 +0300

    refactor authenticate() in the login controller -- change return type from AuthenticationResponse to boolean

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/LoginController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/LoginController.java
@@ -1,16 +1,20 @@
 package com.example.RestOAuth2JPA.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.RestOAuth2JPA.DTO.entities.classModels.AuthenticationRequest;
 import com.example.RestOAuth2JPA.DTO.entities.classModels.AuthenticationResponse;
 import com.example.RestOAuth2JPA.services.AuthService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
@@ -29,9 +33,14 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public String authenticate(/*@RequestBody*/ AuthenticationRequest authenticationRequest, Model model) throws Exception {
-        AuthenticationResponse response = _authService.authenticate(authenticationRequest);
-        model.addAttribute("authToken", response.getResponseToken());
-        return "token_response.html";
+    public RedirectView authenticate(/*@RequestBody*/ AuthenticationRequest authenticationRequest, HttpServletRequest request) throws Exception {
+        boolean isAuthenticated = _authService.authenticate(authenticationRequest, request);
+        if (isAuthenticated) {
+            var auth = SecurityContextHolder.getContext().getAuthentication();
+            return _authService.redirectView("http://localhost:8080/home");
+        }
+        else {
+            return _authService.redirectView("http://localhost:8080/login");
+        }
     }
 }
