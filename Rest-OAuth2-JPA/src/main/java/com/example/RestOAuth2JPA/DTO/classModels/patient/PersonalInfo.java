648dc51ee67ddb96d8d1c96947c87ff70d6981c4
Martin142214 <mi_kalonkin@mail.bg>
2023-09-07 12:57:18 +0300

    add PersonalInfo class model -- wraps personal data in Patient entity

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/PersonalInfo.java
@@ -0,0 +1,12 @@
+package com.example.RestOAuth2JPA.DTO.classModels.patient;
+
+public class PersonalInfo {
+    private String firstName;
+
+    private String lastName;
+
+    private Address address;
+
+    //ЕГН
+    private Integer verificationCode;
+}
