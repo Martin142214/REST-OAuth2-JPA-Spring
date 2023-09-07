ce401ab5e2b85de9640b3dba0b9e53ab010a5c27
Martin142214 <mi_kalonkin@mail.bg>
2023-09-07 13:08:32 +0300

    add NoteData class model

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/NoteData.java
@@ -0,0 +1,11 @@
+package com.example.RestOAuth2JPA.DTO.classModels.patient;
+
+import com.example.RestOAuth2JPA.enums.PlaceOfTreatment;
+
+public class NoteData {
+    private String diagnosis;
+
+    private PlaceOfTreatment placeOfTreatment;
+
+    private String description;
+}
