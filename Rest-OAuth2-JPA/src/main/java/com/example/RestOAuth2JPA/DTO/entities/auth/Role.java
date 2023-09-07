5c0895bd3c72138894bef12683986bdf3d4248bd
Martin142214 <mi_kalonkin@mail.bg>
2023-09-07 17:31:54 +0300

    add Users and Roles entity tables

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/auth/Role.java
@@ -0,0 +1,63 @@
+package com.example.RestOAuth2JPA.DTO.entities.auth;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ROLES")
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "ROLES_PRIVILEGES", 
+               joinColumns = @JoinColumn(name = "role_id"),
+               inverseJoinColumns = @JoinColumn(name = "privilege_id"))
+    private Collection<Privilege> privileges;
+
+    public UUID getId() {
+      return id;
+    }
+
+    public void setId(UUID id) {
+      this.id = id;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public Collection<Privilege> getPrivileges() {
+      return privileges;
+    }
+
+    public Role() {
+
+    }
+
+    public Role(String name) {
+      this.name = name;
+    }
+
+    public void setPrivileges(Collection<Privilege> privileges) {
+      this.privileges = privileges;
+    }
+}
---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/auth/User.java
@@ -0,0 +1,103 @@
+package com.example.RestOAuth2JPA.DTO.entities.auth;
+
+import java.util.UUID;
+
+import org.springframework.context.annotation.Role;
+
+import com.example.RestOAuth2JPA.DTO.entities.Patient;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 24)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 24)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public User(String username, String password, String email, Role role, Patient patient) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.patient = patient;
+    }
+
+    public User() {
+        
+    }
+}
