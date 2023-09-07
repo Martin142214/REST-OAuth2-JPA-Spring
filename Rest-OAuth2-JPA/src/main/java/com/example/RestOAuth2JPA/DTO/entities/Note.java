4026c2dfb71c69261eb8d0c9fb64f837d0b4dcec
Martin142214 <mi_kalonkin@mail.bg>
2023-09-07 17:30:05 +0300

    generate getters and setters for Note entity

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/Note.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/Note.java
@@ -3,12 +3,20 @@ package com.example.RestOAuth2JPA.DTO.entities;
 import java.io.Serializable;
 import java.util.UUID;
 
+import com.example.RestOAuth2JPA.DTO.classModels.patient.NoteData;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "NOTES")
 //Many To Many table
 public class Note implements Serializable {
 
@@ -16,9 +24,46 @@ public class Note implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private UUID patientId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
 
-    private UUID doctorId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
 
+    @Column(name = "data")
     private NoteData data;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public NoteData getData() {
+        return data;
+    }
+
+    public void setData(NoteData data) {
+        this.data = data;
+    }
 }
