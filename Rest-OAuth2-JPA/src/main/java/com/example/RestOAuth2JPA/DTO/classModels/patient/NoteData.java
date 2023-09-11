ae4f83f880d3f2c280d37f7d21967fee6a378fbe
Martin142214 <mi_kalonkin@mail.bg>
2023-09-11 13:11:47 +0300

    refactor NoteData class -- set the classModel to Entity type -- set OneToOne relational column for note_id

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/NoteData.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/NoteData.java
@@ -1,8 +1,27 @@
 package com.example.RestOAuth2JPA.DTO.classModels.patient;
 
+import com.example.RestOAuth2JPA.DTO.entities.Note;
 import com.example.RestOAuth2JPA.enums.PlaceOfTreatment;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class NoteData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "note_id", referencedColumnName = "id")
+    private Note note;
+
     private String diagnosis;
 
     private PlaceOfTreatment placeOfTreatment;
