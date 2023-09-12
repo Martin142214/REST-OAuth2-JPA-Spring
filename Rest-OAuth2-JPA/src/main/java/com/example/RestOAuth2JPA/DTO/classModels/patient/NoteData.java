1951681297bb23e54671a1645b57ae6eaa940784
Martin142214 <mi_kalonkin@mail.bg>
2023-09-12 15:36:19 +0300

    add some additional annotations for NoteData

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/NoteData.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/NoteData.java
@@ -5,13 +5,18 @@ import com.example.RestOAuth2JPA.enums.PlaceOfTreatment;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
+@Table(name = "note_data")
 public class NoteData {
 
     @Id
@@ -22,9 +27,12 @@ public class NoteData {
     @JoinColumn(name = "note_id", referencedColumnName = "id")
     private Note note;
 
+    @Transient
     private String diagnosis;
 
+    @Enumerated(EnumType.STRING)
     private PlaceOfTreatment placeOfTreatment;
 
+    @Transient
     private String description;
 }
