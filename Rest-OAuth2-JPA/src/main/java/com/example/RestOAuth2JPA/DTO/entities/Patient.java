e5d3c11841658ada5671696cd6853163d03c5d9b
Martin142214 <mi_kalonkin@mail.bg>
2023-09-12 15:32:10 +0300

    refactor Patient entity -- add Column annotation to Status property -- change identity type of Patient entity

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/Patient.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/Patient.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.UUID;
 
-import com.example.RestOAuth2JPA.DTO.classModels.patient.Address;
 import com.example.RestOAuth2JPA.DTO.classModels.patient.PersonalInfo;
 import com.example.RestOAuth2JPA.enums.Status;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,25 +21,29 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.websocket.OnError;
 
 @Entity
-@Table(name = "PATIENTS")
+@Table(name = "patients")
 public class Patient implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "person")
     private PersonalInfo personalInfo;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "patient_status")
     private Status status;
 
     //many patients belong to one doctor
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "doctor_id", referencedColumnName = "id")
     private Doctor doctor;
+
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "patient")
     private Collection<Note> notes;
@@ -51,11 +57,11 @@ public class Patient implements Serializable {
         
     }
 
-    public UUID getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
