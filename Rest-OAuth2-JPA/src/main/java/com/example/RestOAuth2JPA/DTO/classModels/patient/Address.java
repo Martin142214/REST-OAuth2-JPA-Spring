6f40136e64706d4a190c590671c3f84134638e3d
Martin142214 <mi_kalonkin@mail.bg>
2023-09-12 10:52:55 +0300

    refactor Address entity -- add OneToOne relation to PersonalInfo via personalData_id column

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/Address.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/classModels/patient/Address.java
@@ -1,20 +1,64 @@
 package com.example.RestOAuth2JPA.DTO.classModels.patient;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "personalData_id", referencedColumnName = "id")
+    private PersonalInfo personalInfo;
 
     private String street;
 
     private String city;
 
     private Integer postCode;
-
+    
+    public Address() {}
+    
     public Address(String street, String city, Integer postCode) {
         this.street = street;
         this.city = city;
         this.postCode = postCode;
     }
 
-    public Address() {}
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(Integer postCode) {
+        this.postCode = postCode;
+    }
+
 
     
 }
