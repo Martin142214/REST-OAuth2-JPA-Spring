6e21d8a074fbe8e7ebe86da8ca723946ba2c610e
Martin142214 <mi_kalonkin@mail.bg>
2023-09-13 14:48:11 +0300

    add FileDB class to gather image data

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/FileDB.java
@@ -0,0 +1,43 @@
+package com.example.RestOAuth2JPA.DTO.entities;
+
+import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_images")
+public class FileDB {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+  
+    @Column(name = "imageName", nullable = false)
+    public String name;
+
+    @Column(name = "imageExtension", nullable = false)
+    public String type;
+
+    @Column(name = "imagePath", nullable = false)
+    public String imagePath; 
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "profileImage")
+    private User user;
+
+    public FileDB() {
+    }
+
+    public FileDB(String name, String type, String imagePath) {
+        this.name = name;
+        this.type = type;
+        this.imagePath = imagePath;
+    }
+}
