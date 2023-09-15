be3535fd3641863ef43ff939727f5fdf7039b39c
Martin142214 <mi_kalonkin@mail.bg>
2023-09-15 15:14:15 +0300

    change type of Primary key in UsersRepo -- from UUID to Long

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
@@ -1,11 +1,9 @@
 package com.example.RestOAuth2JPA.DTO.repositories.auth;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
 
-public interface IUsersRepository extends JpaRepository<User, UUID> {
+public interface IUsersRepository extends JpaRepository<User, Long> {
     
     User findByUsername(String username);
     
