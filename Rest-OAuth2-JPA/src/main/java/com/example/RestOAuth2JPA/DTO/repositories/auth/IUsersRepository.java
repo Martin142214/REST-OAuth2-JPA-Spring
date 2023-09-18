278307bcac925115f0c9b3d0f34abaf98e82eeb0
Martin142214 <mi_kalonkin@mail.bg>
2023-09-18 15:42:20 +0300

    change findByUsername(username) return type -- from User to Optional<User>

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
@@ -1,11 +1,13 @@
 package com.example.RestOAuth2JPA.DTO.repositories.auth;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
 
 public interface IUsersRepository extends JpaRepository<User, Long> {
     
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     
     User findByEmail(String email);
 }
