a078025f8b9b3f2b4cc07eb9745d98e2332f14e1
Martin142214 <mi_kalonkin@mail.bg>
2023-09-25 18:17:48 +0300

    add methods and comment unused in Users repo

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/auth/IUsersRepository.java
@@ -3,11 +3,19 @@ package com.example.RestOAuth2JPA.DTO.repositories.auth;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
 
 public interface IUsersRepository extends JpaRepository<User, Long> {
     
-    Optional<User> findByUsername(String username);
+    //@Query("SELECT u FROM User WHERE u.username = :username")
+    public User findByUsername(String username);
     
-    User findByEmail(String email);
+    public User findByEmail(String email);
+
+    //@Query("SELECT case when count(c)> 0 then true else false end FROM User u WHERE lower(u.username) like lower(:username)")
+    //@Query(value = "select if(count(name) > 0, 'true', 'false') from User user where user.name = :username", nativeQuery = true)
+    //public Boolean userExistsByUsername(String username);
 }
