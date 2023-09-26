e916c31457a0c7f59c37a7d3f92b1db7f9b95a6a
Martin142214 <mi_kalonkin@mail.bg>
2023-09-26 16:39:11 +0300

    add /profile endpoint in PatientUserController

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/PatientUserController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/PatientUserController.java
@@ -4,10 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.services.auth.UserService;
 
 @Controller
 @RequestMapping(value = "/user/patient")
 public class PatientUserController {
+
+    @Autowired UserService userService;
     
     @Autowired
     public PatientUserController() {
@@ -15,7 +21,12 @@ public class PatientUserController {
     }
 
     @GetMapping("/profile")
-    public String patient_profile_page() {
-        return "user_patient/patient_profile_page.html";
+    public ModelAndView patient_profile_page() {
+        ModelAndView modelAndView = new ModelAndView("user_patient/patient_profile_page.html");
+        User user = userService.get_currently_loggedInUser();
+        if (user.isEnabled()) {
+            modelAndView.addObject("userPatient", user);         
+        }
+        return modelAndView;
     }
 }
