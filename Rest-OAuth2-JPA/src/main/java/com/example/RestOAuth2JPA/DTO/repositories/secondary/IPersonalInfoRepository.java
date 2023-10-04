c3a53cbccf936938e523bbe1a59bd7ebd22f058f
Martin142214 <mi_kalonkin@mail.bg>
2023-10-04 19:35:45 +0300

    deleted classes

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/repositories/secondary/IPersonalInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.RestOAuth2JPA.DTO.repositories.secondary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.RestOAuth2JPA.DTO.entities.patient.Personal_patient_info;
-
-public interface IPersonalInfoRepository extends JpaRepository<Personal_patient_info, Long> {
-    
-}
