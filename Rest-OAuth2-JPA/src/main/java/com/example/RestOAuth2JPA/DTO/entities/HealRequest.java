b79d15d9006cdd15eb69d71bb67bf105d7e35768
Martin142214 <mi_kalonkin@mail.bg>
2023-10-06 11:29:47 +0300

    add HealRequest class -- id - Long -- User patient -- User doctor -- HealRequestStatus requestStatus -- String requestReason -- Date requestDate

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/DTO/entities/HealRequest.java
@@ -0,0 +1,85 @@
+package com.example.RestOAuth2JPA.DTO.entities;
+
+import java.sql.Date;
+
+import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.enums.HealRequestStatus;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "heal_requests")
+public class HealRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+    private User patient;
+
+    //many patients belong to one doctor
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "doctor_id", referencedColumnName = "id")
+    private User doctor;
+
+    
+    @Column(name = "status", nullable = false)
+    private HealRequestStatus requestStatus;
+
+    @Column(name = "reason")
+    private String requestReason;
+    
+    @Column(name = "date", nullable = false)
+    private Date requestDate;
+    
+    public HealRequest(User patient, User doctor, HealRequestStatus requestStatus, Date requestDate) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.requestStatus = requestStatus;
+        this.requestDate = requestDate;
+    }
+    
+    public User getPatient() {
+        return patient;
+    }
+
+    public void setPatient(User patient) {
+        this.patient = patient;
+    }
+
+    public User getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(User doctor) {
+        this.doctor = doctor;
+    }
+
+    public HealRequestStatus getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(HealRequestStatus requestStatus) {
+        this.requestStatus = requestStatus;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public HealRequest() {
+        
+    }
+
+    public HealRequest(HealRequestStatus status) {
+        this.requestStatus = status;
+    }
+}
+
