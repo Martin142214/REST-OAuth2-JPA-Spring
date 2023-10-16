be02690a0d5ae3abed2b6eee2ad3eb984900b183
Martin142214 <mi_kalonkin@mail.bg>
2023-10-16 15:09:33 +0300

    add patient_profile_page.js -- sets color based on the status of the patient

--- Rest-OAuth2-JPA/src/main/resources/static/scripts/user_patient/patient_profile_page.js
+++ Rest-OAuth2-JPA/src/main/resources/static/scripts/user_patient/patient_profile_page.js
@@ -2,3 +2,25 @@ function redirectSelect(element)
   {
      window.location = element.value;
   }
+
+function setColorToPatientStatus() {
+   var pTag = document.getElementById("patientStatus");
+   pTag.style.borderRadius = "5px";
+   pTag.style.color = "white";
+   var status = pTag.innerHTML.toLowerCase();
+   switch (status) {
+      case "unknown":
+         pTag.style.backgroundColor = 'grey';
+         break;
+      case "healthy":
+         pTag.style.backgroundColor = 'green';
+         break;
+      case "sick":
+         pTag.style.backgroundColor = 'red';
+         break;
+      case "in_treatment":
+         pTag.style.backgroundColor = 'orange';
+         break;
+      
+   }
+}
