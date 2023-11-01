86b17abd98491a0f90b64251bd6ab545c79bd147
Martin142214 <mi_kalonkin@mail.bg>
2023-11-01 12:01:32 +0200

    add changes in UserService class -- divide methods in 3 sections - for general use, Patients and Doctors -- implement 3 interfaces with different method purposes

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/UserService.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/UserService.java
@@ -1,6 +1,20 @@
 package com.example.RestOAuth2JPA.services.auth;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -11,7 +25,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.DTO.entities.doctor.Doctor;
+import com.example.RestOAuth2JPA.DTO.entities.patient.Patient;
 import com.example.RestOAuth2JPA.DTO.repositories.IDoctorRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.IPatientRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.auth.IUsersRepository;
@@ -20,9 +38,12 @@ import com.example.RestOAuth2JPA.DTO.repositories.secondary.IFileDBRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.secondary.IPersonalPatientInfoRepository;
 import com.example.RestOAuth2JPA.components.classModels.UserModelData;
 import com.example.RestOAuth2JPA.services.additional.RedirectHandler;
+import com.example.RestOAuth2JPA.services.auth.Interfaces.IUserDoctorService;
+import com.example.RestOAuth2JPA.services.auth.Interfaces.IUserPatientService;
+import com.example.RestOAuth2JPA.services.auth.Interfaces.IUserService;
 
 @Service
-public class UserService {
+public class UserService implements IUserService, IUserPatientService, IUserDoctorService {
 
     @Autowired RoleService roleService;
 
@@ -46,13 +67,16 @@ public class UserService {
         
     }
 
-    private boolean authenticated() {
+    // METHODS FOR GENERAL USE
+    @Override
+    public boolean authenticated() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         
         return authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken);
     }
 
-    public User get_currently_loggedInUser() {
+    @Override
+    public User getCurrentlyLoggedInUser() {
 
         if (authenticated()) {
             return _usersRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
@@ -62,11 +86,13 @@ public class UserService {
         }
     }
 
+    @Override
     public Optional<User> findById(Long id) {
         return _usersRepository.findById(id);
     }
 
-    public User get_user_by_username(String username) {
+    @Override
+    public User getUserByUsername(String username) {
         return _usersRepository.findByUsername(username);
     }
 
@@ -84,10 +110,31 @@ public class UserService {
                                          .collect(Collectors.toList());
     }
 
-    public List<User> get_all_patient_users() {
-        return _usersRepository.findAll().stream()
+    @Override
+    public Collection<User> getAllPatientUsers() {
+        //TODO check this func if works
+        // check the linkedList<> shallow copy - LinkedList.clone()
+        ListIterator<User> iterator = _usersRepository.findAll().listIterator();
+        Collection<User> patientUsers = new LinkedList<>();
+        
+        while (iterator.hasNext()) {
+            var currentUser = iterator.next();
+            if (currentUser.getRole().getName().equalsIgnoreCase("ROLE_PATIENT")) {
+                patientUsers.add(currentUser);
+            }
+        }
+        return patientUsers;
+        /*return _usersRepository.findAll().stream()
                                          .filter(user -> user.getRole().getName().equalsIgnoreCase("ROLE_PATIENT"))
-                                         .collect(Collectors.toList());
+                                         .collect(Collectors.toList());*/
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return _usersRepository.findById(id);
+    }
+
+    public void updateUser(User user) {
+        _usersRepository.save(user);
     }
 
     public boolean emailExist(String email){
@@ -106,6 +153,37 @@ public class UserService {
         return false;
     }
 
+    /**
+     * print an collection of generic items
+     * @param collection Collection<T> to be printed
+     * @throws IndexOutOfBoundException
+     */
+
+    /*public <T> void print(final Collection<T> collection) {
+        for (final T elemenT : collection) {
+            Arrays.stream(collection.toArray()).forEach(element -> {
+                var el = element.toString();
+            });
+        }
+    }
+        */
+    public void outputStream() {
+        final Path path = new File("/ss/").toPath();
+        try (ObjectOutputStream oStream = new ObjectOutputStream(Files.newOutputStream(path))) {
+            Doctor doctor = new Doctor();
+            oStream.writeObject(doctor);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
+        try (ObjectInputStream inputStream = new ObjectInputStream(Files.newInputStream(path))) {
+            Doctor doctor = (Doctor) inputStream.readObject();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
     public void create_new_user_and_saveDB(UserModelData data) {
                 
         var user = data.setCompleteUserData();
@@ -122,6 +200,51 @@ public class UserService {
             personalInfoRepository.save(user.getPatient().getPersonalInfo());
             patientRepository.save(user.getPatient());    
         }
+
         this.save_new_user(user);
     }
+    
+    
+    //METHODS FOR PATIENT USERS
+
+    @Override
+    public List<User> convertCollectionOfPatientEntitiesToListOfUsers(final Collection<Patient> patients) {
+        //TODO CHECK how can I map all patient entities to their users without iteratng all the database entitites
+        List<User> userPatients = new ArrayList<>();
+        for (Patient patient : patients) {
+            userPatients.add(patient.getUser());
+        } 
+        return userPatients;
+    }
+
+    @Override
+    public ModelAndView getAllPatientUsersForDoctorAndReturnModelAndView() {
+
+        Collection<Patient> allPatientsToDoctorUser = this.getCurrentlyLoggedInUser().getDoctor().getPatients();
+        List<User> doctorUserPatients = this.convertCollectionOfPatientEntitiesToListOfUsers(allPatientsToDoctorUser);
+
+         ModelAndView modelAndView = new ModelAndView("user_doctor/doctor_patients_list.html");
+         modelAndView.addObject("doctorPatients", doctorUserPatients);
+         modelAndView.addObject("hasNoPatients", allPatientsToDoctorUser.size() > 0 ? false : true);
+         return modelAndView;
+    }
+
+    //METHODS FOR DOCTOR USERS
+
+    @Override
+    public Map<Long, String> addDoctorInfoDataInHashMap() {
+        Map<Long, String> doctorUsers = new HashMap<>();
+        for (int i = 0; i < this.get_all_doctor_users().size(); i++) {
+            var doctorEntity = this.get_all_doctor_users().get(i).getDoctor();
+            doctorUsers.put(this.get_all_doctor_users().get(i).getId(), doctorEntity.getPersonalInfo().getFirstName() + " " +
+                                                                               doctorEntity.getPersonalInfo().getLastName() + ", " + 
+                                                                               doctorEntity.getPositionName());      
+        }
+        return doctorUsers;
+    }
+
+
+
+    
+
 }
