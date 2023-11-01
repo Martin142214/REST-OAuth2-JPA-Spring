d838648892124d488b22784a86c62cc0dbe00584
Martin142214 <mi_kalonkin@mail.bg>
2023-11-01 11:57:09 +0200

    commit

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/Interfaces/IUserDoctorService.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/Interfaces/IUserDoctorService.java
@@ -3,5 +3,6 @@ package com.example.RestOAuth2JPA.services.auth.Interfaces;
 import java.util.Map;
 
 public interface IUserDoctorService {
+    
     Map<Long, String> addDoctorInfoDataInHashMap();
 }
