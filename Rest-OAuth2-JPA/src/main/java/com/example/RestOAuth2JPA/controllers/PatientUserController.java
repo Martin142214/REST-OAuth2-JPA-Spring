6fe47fd74182dce1233de8e0082aab063fbdff07
Martin142214 <mi_kalonkin@mail.bg>
2023-11-01 12:02:19 +0200

    add changes in PatientUserController class

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/PatientUserController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/PatientUserController.java
@@ -10,13 +10,20 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.RestOAuth2JPA.DTO.entities.HealRequest;
+import com.example.RestOAuth2JPA.DTO.entities.Note;
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.DTO.entities.patient.NoteData;
+import com.example.RestOAuth2JPA.DTO.repositories.INotesRepository;
 import com.example.RestOAuth2JPA.enums.HealRequestStatus;
 import com.example.RestOAuth2JPA.enums.HospitalDepartments;
+import com.example.RestOAuth2JPA.enums.Status;
+import com.example.RestOAuth2JPA.services.additional.RedirectHandler;
 import com.example.RestOAuth2JPA.services.auth.UserService;
 import com.example.RestOAuth2JPA.services.fileUpload.FileUploadUtils;
 
@@ -25,34 +32,42 @@ import com.example.RestOAuth2JPA.services.fileUpload.FileUploadUtils;
 public class PatientUserController {
 
     @Autowired UserService userService;
+
+    @Autowired INotesRepository notesRepository;
+
+    @Autowired RedirectHandler redirectHandler;
     
     @Autowired
     public PatientUserController() {
         
     }
 
+    @PostMapping("/profile/{id}/setStatus")
+    @Secured("ROLE_DOCTOR")
+    public RedirectView setStatusToPatient(@PathVariable Long id, String status) {
+        Optional<User> userPatient = userService.findById(id);
+        if (userPatient.isPresent()) {
+            userPatient.get().getPatient().setStatus(Status.valueOf(status));
+        }
+        
+        return redirectHandler.redirectView(redirectHandler.patientProfileUrl + id);
+    }
+
     @GetMapping("/profile")
     @Secured("ROLE_PATIENT")
     public ModelAndView patient_profile_page() {
+        User patient = userService.getCurrentlyLoggedInUser();
+        
         ModelAndView modelAndView = new ModelAndView("user_patient/patient_profile_page.html");
-        User patient = userService.get_currently_loggedInUser();
-        Map<Long, String> doctorUsers = new HashMap<>();
-        for (int i = 0; i < userService.get_all_doctor_users().size(); i++) {
-            var doctorEntity = userService.get_all_doctor_users().get(i).getDoctor();
-            doctorUsers.put(userService.get_all_doctor_users().get(i).getId(), doctorEntity.getPersonalInfo().getFirstName() + " " +
-                                                                               doctorEntity.getPersonalInfo().getLastName() + ", " + 
-                                                                               doctorEntity.getPositionName());      
-        }
-        if (patient.isEnabled()) {
-            modelAndView.addObject("userPatient", patient); 
-            modelAndView.addObject("doctorUsers", doctorUsers); 
-            if (userService.patientHasDoctor(patient)) {
-                modelAndView.addObject("patientHasDoctor", userService.patientHasDoctor(patient));
-                modelAndView.addObject("patientDoctor", patient.getPatient().getDoctor().getUser());
-            }       
-        }
+        modelAndView.addObject("doctorUsers", userService.addDoctorInfoDataInHashMap()); 
+        if (userService.userPatientHasDoctor(patient)) {
+            modelAndView.addObject("patientHasDoctor", userService.userPatientHasDoctor(patient));
+            modelAndView.addObject("patientDoctor", patient.getPatient().getDoctor().getUser());
+        }       
+        
         return modelAndView;
     }
+
 
     @GetMapping("/profile/{id}")
     @Secured("ROLE_DOCTOR")
@@ -61,6 +76,20 @@ public class PatientUserController {
         Optional<User> userPatient = userService.findById(id);
         if (userPatient.isPresent()) {
             modelAndView.addObject("patientInfo", userPatient.get());
+            modelAndView.addObject("doctorUser", userService.getCurrentlyLoggedInUser());
+            var notesForPatient = notesRepository.findAll().stream().filter(note -> note.getDoctor().getId().equals(userService.getCurrentlyLoggedInUser().getDoctor().getId()) &&
+                                                                                    note.getPatient().getId().equals(userPatient.get().getPatient().getId())).toList();
+            int i = 0; 
+            //TODO have stackoverflow exception because of many relational data in notes entity (patient, doctor)
+            /*Map<Long, NoteData> patientNotes = new HashMap<>();
+            for (Note note : notesForPatient) {
+                patientNotes.put(note.getId(), note.getData());
+            }*/
+            if (!notesForPatient.isEmpty()) {
+                String patientName = userPatient.get().getPatient().getPersonalInfo().getFirstName() + " " + userPatient.get().getPatient().getPersonalInfo().getLastName();
+                modelAndView.addObject("patientFullName", patientName);
+                modelAndView.addObject("patientNotes", notesForPatient);       
+            }
             /*modelAndView.addObject("patientId", userService.get_currently_loggedInUser().getId()); 
             var doctorDepartment = HospitalDepartments.valueOf(userDoctor.get().getDoctor().getDepartment()).getDepartmentName();   
             modelAndView.addObject("department", doctorDepartment);
