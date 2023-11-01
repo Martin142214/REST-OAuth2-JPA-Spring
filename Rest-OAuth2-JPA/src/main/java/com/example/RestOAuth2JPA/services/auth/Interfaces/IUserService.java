589d9d74dfa8f600e3048e8d3ed9480b50e93416
Martin142214 <mi_kalonkin@mail.bg>
2023-11-01 11:46:29 +0200

    add IUserService interface -- serves as a contract for mandatory methods in UserService @Service -- contains default method implementations for general use

---
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/Interfaces/IUserService.java
@@ -0,0 +1,35 @@
+package com.example.RestOAuth2JPA.services.auth.Interfaces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.DTO.entities.patient.Patient;
+
+public interface IUserService {
+
+    boolean authenticated();
+
+    User getCurrentlyLoggedInUser();
+
+    Optional<User> findById(Long id);
+
+    User getUserByUsername(String username);
+
+    Collection<User> getAllPatientUsers();
+
+    default boolean userPatientHasDoctor(User patient) {
+        return patient.getPatient().getDoctor() != null ? true : false;
+    }
+
+    default List<User> getAllPatientsForUserDoctor(User doctor) {
+        List<User> doctorUserPatients = new ArrayList<>();
+        for (Patient patient : doctor.getDoctor().getPatients()) {
+            doctorUserPatients.add(patient.getUser());
+        } 
+        
+        return doctorUserPatients;
+    }
+}
