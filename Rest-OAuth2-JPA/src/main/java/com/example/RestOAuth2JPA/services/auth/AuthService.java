d9bf184c59e63dcd7d4ea397e2daad940cf409cc
Martin142214 <mi_kalonkin@mail.bg>
2023-11-02 14:52:44 +0200

    changes in AuthService @Service -- DI of userService, roleService and fileDbService -- createNewAdminUserAndSaveToDB() added

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/AuthService.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/services/auth/AuthService.java
@@ -1,18 +1,20 @@
 package com.example.RestOAuth2JPA.services.auth;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
+
+import com.example.RestOAuth2JPA.DTO.entities.FileDB;
+import com.example.RestOAuth2JPA.DTO.entities.auth.Role;
+import com.example.RestOAuth2JPA.DTO.entities.auth.User;
+import com.example.RestOAuth2JPA.components.classModels.auth.User_login;
+import com.example.RestOAuth2JPA.services.fileUpload.FileDbService;
+import com.example.RestOAuth2JPA.services.fileUpload.FileUploadUtils;
 
 //import com.example.RestOAuth2JPA.services.userDetails.UserDetailsServiceImplementation;
 
@@ -27,38 +29,51 @@ public class AuthService {
 
     //@Autowired UserDetailsServiceImplementation userDetailsService;
 
+    @Autowired UserService userService;
+
+    @Autowired RoleService roleService;
+
+    @Autowired FileDbService fileDbService;
+
     @Autowired JwtUtil jwtUtil;
 
     @Autowired
-    public AuthService() {
-        
+    public AuthService(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
     }
 
-    /*public boolean authenticate(AuthenticationRequest authenticationRequest, HttpServletRequest request) throws Exception {
-        var username = authenticationRequest.getUsername();
-        var password = authenticationRequest.getPassword();
+    public void createNewAdminUserAndSaveToDB(final User_login userLoginData, final MultipartFile userImage) {
+        /*Privilege readPrivilege = privilegeService.createPrivilegeIfNotFound("READ_PRIVILEGE");
+        Privilege createPrivilege = privilegeService.createPrivilegeIfNotFound("CREATE_PRIVILEGE");
+        Privilege updatePrivilege = privilegeService.createPrivilegeIfNotFound("UPDATE_PRIVILEGE");
+        Privilege deletePrivilege = privilegeService.createPrivilegeIfNotFound("DELETE_PRIVILEGE");
 
-        var auth = SecurityContextHolder.getContext().getAuthentication();
+        List<Privilege> adminPrivileges = Arrays.asList(readPrivilege, createPrivilege, updatePrivilege, deletePrivilege);
+        roleService.createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
+        roleService.createRoleIfNotFound("ROLE_PATIENT", Arrays.asList(readPrivilege));
+        roleService.createRoleIfNotFound("ROLE_DOCTOR", Arrays.asList(readPrivilege));*/
+
+        // the above is done; saved in the DB
+
+        // get all users with all their parent entities data
+        /*var users = userService.get_all_users().stream()
+                                               .filter(u -> !u.isAdmin())
+                                               .collect(Collectors.toList());*/
         
-        if(this.authenticate(username, password)){
-            var auth1 = SecurityContextHolder.getContext().getAuthentication();
-            final UserDetails userDetails = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-    
-            var token = jwtUtil.generateToken(userDetails);
-            if (isValidTokenForAuth(username, token, request)) {
-                var auth2 = SecurityContextHolder.getContext().getAuthentication();
-                return true;
-            }
+        final Role adminRole = roleService.findByName("ROLE_ADMIN");
+        User user = userService.createAdminUserAndReturn(userLoginData, adminRole);
+
+        FileDB userImageFile = FileUploadUtils.mkdir_and_upload_file_and_return_imagePath(userLoginData.getUsername(), adminRole, userImage);
+        user.setProfileImage(userImageFile);
+
+        try {
+            fileDbService.saveNewFileDB(userImageFile);
+            this.userService.saveNewOrUpdateUser(user);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        
-
-        //Cookie cookie = new Cookie("Auth", token);
-        //cookie.setMaxAge(30*6);
-        //response.addCookie(cookie);
-
-        return false;
-        //return new AuthenticationResponse(token);
-    }*/
+    }
 
     private boolean authenticate(String username, String password) throws Exception {
         try {
