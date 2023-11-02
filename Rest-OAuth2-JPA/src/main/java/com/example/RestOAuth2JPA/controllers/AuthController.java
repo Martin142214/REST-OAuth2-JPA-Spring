8c29b56dde63858d8e64018c6854f2d4e31f3ffc
Martin142214 <mi_kalonkin@mail.bg>
2023-11-02 14:54:43 +0200

    add refactoring changes in AuthController class -- processRegister() is shortened

--- Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/AuthController.java
+++ Rest-OAuth2-JPA/src/main/java/com/example/RestOAuth2JPA/controllers/AuthController.java
@@ -1,14 +1,9 @@
 package com.example.RestOAuth2JPA.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,32 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
-import com.example.RestOAuth2JPA.DTO.entities.FileDB;
-import com.example.RestOAuth2JPA.DTO.entities.auth.Role;
 import com.example.RestOAuth2JPA.DTO.entities.auth.User;
 import com.example.RestOAuth2JPA.DTO.repositories.IPatientRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.auth.IUsersRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.secondary.IAddressRepository;
-import com.example.RestOAuth2JPA.DTO.repositories.secondary.IFileDBRepository;
 import com.example.RestOAuth2JPA.DTO.repositories.secondary.IPersonalPatientInfoRepository;
 import com.example.RestOAuth2JPA.components.classModels.UserModelData;
+import com.example.RestOAuth2JPA.components.classModels.auth.User_login;
 import com.example.RestOAuth2JPA.services.additional.RedirectHandler;
+import com.example.RestOAuth2JPA.services.auth.AuthService;
 import com.example.RestOAuth2JPA.services.auth.PrivilegeService;
 import com.example.RestOAuth2JPA.services.auth.RoleService;
 import com.example.RestOAuth2JPA.services.auth.UserService;
-import com.example.RestOAuth2JPA.services.fileUpload.FileUploadUtils;
-import com.example.RestOAuth2JPA.services.userDetails.CustomUserDetails;
-
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthController {
 
-    //@Autowired AuthService _authService;
-
     @Autowired UserService userService;
 
-    @Autowired IUsersRepository usersRepository;
+    @Autowired AuthService authService;
     
     @Autowired RoleService roleService;
     
@@ -55,7 +44,6 @@ public class AuthController {
 
     @Autowired IAddressRepository addressRepository;
 
-    @Autowired IFileDBRepository fileDBRepository;
 
     @Autowired PasswordEncoder passwordEncoder;
 
@@ -79,119 +67,18 @@ public class AuthController {
     @PostMapping("/admin/register")
     public RedirectView processRegister(String username, String email, String password,
                                         @RequestParam("userImage") MultipartFile userImage, HttpServletRequest request) {
-
-        /*Privilege readPrivilege = privilegeService.createPrivilegeIfNotFound("READ_PRIVILEGE");
-        Privilege createPrivilege = privilegeService.createPrivilegeIfNotFound("CREATE_PRIVILEGE");
-        Privilege updatePrivilege = privilegeService.createPrivilegeIfNotFound("UPDATE_PRIVILEGE");
-        Privilege deletePrivilege = privilegeService.createPrivilegeIfNotFound("DELETE_PRIVILEGE");
-
-        List<Privilege> adminPrivileges = Arrays.asList(readPrivilege, createPrivilege, updatePrivilege, deletePrivilege);
-        roleService.createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
-        roleService.createRoleIfNotFound("ROLE_PATIENT", Arrays.asList(readPrivilege));
-        roleService.createRoleIfNotFound("ROLE_DOCTOR", Arrays.asList(readPrivilege));*/
-
-        // the above is done; saved in the DB
-
-        // get all users with all their parent entities data
-        /*var users = userService.get_all_users().stream()
-                                               .filter(u -> !u.isAdmin())
-                                               .collect(Collectors.toList());*/
         
-        User user = new User();
-        
-        Role adminRole = roleService.findByName("ROLE_ADMIN");
-        if (adminRole == null) {
-            throw new Error("Cannot register admin role without the role itself. Please see if the ADMIN_ROLE persist in dbo.roles");
-        }
+        User_login userLoginData = new User_login(username, password);
+        userLoginData.setEmail(email);
+        authService.createNewAdminUserAndSaveToDB(userLoginData, userImage);
 
-        if (username.length() >= 24) {
-            throw new Error("Cannot save username with more than 24 characters");
-        }
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.encode(password));
-        user.setEmail(email);
-        user.setRole(adminRole);
-        user.setEnabled(true);
-        user.setTokenExpired(false);
-        user.setAdmin(true);
-
-        //TODO add FILEDB convert and save uploaded images in the project
-        FileDB userImageFile = FileUploadUtils.mkdir_and_upload_file_and_return_imagePath(username, adminRole, userImage);
-        user.setProfileImage(userImageFile);
-
-        fileDBRepository.save(userImageFile);
-        this.userService.save_new_user(user);
-
-        return redirectHandler.redirectView("http://localhost:8080/user/register");
+        return redirectHandler.redirectView("http://localhost:8080/user/login");
     }
 
     @RequestMapping(value = "/user/register", method = RequestMethod.GET)
     public String return_register_form() {
         return "/auth/registration_form.html";
     }
-
-    /*@RequestMapping(value = "/user/patient/register", method = RequestMethod.POST)
-    public RedirectView register_new_user_patient(@ModelAttribute User user, 
-                                                   String firstName, String lastName, String verificationCode, 
-                                                   String street, String city, Integer postCode,
-                                                   @RequestParam("userImage") MultipartFile userImage) {
-
-        if (userService.emailExist(user.getEmail())) {
-            String email = user.getEmail();
-            try {
-                throw new Exception
-                  ("There is an account with that email adress: " + email);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        else if(userService.usernameExist(user.getUsername())){
-            String username = user.getUsername();
-            try {
-                throw new Exception
-                  ("There is an account with that username: " + username);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        
-        try {
-            Address address = new Address(street, city, postCode);
-            
-            Personal_patient_info info = new Personal_patient_info(firstName, lastName, address, verificationCode);
-
-            //address.setPersonalInfo(info);
-            //1 info.setAddress(address);
-            
-            Patient patient = new Patient(info, Status.Unknown);
-            patient.setNotes(new ArrayList<>());
-
-            //info.setPerson(patient);
-            //1 patient.setPersonalInfo(info);
-            
-            String encodedPass = passwordEncoder.encode(user.getPassword());
-            user.setPassword(encodedPass);
-            user.setEnabled(true);
-            user.setRole(roleService.findByName("ROLE_PATIENT"));
-            FileDB userImageFile = FileUploadUtils.mkdir_and_upload_file_and_return_imagePath(user.getUsername(), user.getRole(), userImage);
-            user.setProfileImage(userImageFile);
-            
-            //patient.setUser(user);
-            user.setPatient(patient);
-            int i = 0;
-
-            fileDBRepository.save(userImageFile);
-            addressRepository.save(address); 
-            personalInfoRepository.save(info);
-            patientRepository.save(patient);
-            userService.save_new_user(user);
-        } 
-        catch (Exception e) {
-            throw new Error(e.getMessage());
-        }
-        
-        return redirectHandler.redirectView(patientLoginPageUrl);
-    }*/
 
     @RequestMapping(value = "/user/register", method = RequestMethod.POST)
     public RedirectView register_new_user(String firstName, String lastName, String verificationCode, 
@@ -225,7 +112,7 @@ public class AuthController {
             user.setPassword(encodedPass);
 
             UserModelData userModelData = new UserModelData(firstName, lastName, verificationCode, street, city, postCode, userImage, isUserDoctor, phoneNumber, department, positionName, user);
-            userService.create_new_user_and_saveDB(userModelData);
+            userService.createNewUserAndSaveDB(userModelData);
             int i = 0;
 
         } 
@@ -241,18 +128,6 @@ public class AuthController {
         return "/auth/login.html";
 
     }
-
-    /*@RequestMapping(value = "/user/patient/login", method = RequestMethod.GET)
-    public String login_user_patient() {
-        return "/auth/login_user_patient.html";
-
-    }
-
-    @RequestMapping(value = "/user/doctor/login", method = RequestMethod.GET)
-    public String login_user_doctor() {
-        return "/auth/login_user_doctor.html";
-
-    }*/
 
     /*@RequestMapping(value = "/user/doctor/login", method = RequestMethod.POST)
     public RedirectView login_user_doctor(String username, @RequestParam("password") String password, 
